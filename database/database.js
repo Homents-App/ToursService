01eda26b4a80fbd420fc0b0fe353e35107fc3f70
Leah Cardon <cardonleah@gmail.com>
2020-12-18 13:54:22 -0700

    Added private IP address of db

--- database/database.js
+++ database/database.js
@@ -2,17 +2,16 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  // user: process.env.PGUSER,
-  // password: process.env.PGPASSWORD,
   user: 'postgres',
   password: 'sdc',
-  host: process.env.PGHOST,
+  host: '172.31.23.189',
   database: 'sdc',
 });
-// eslint-disable-next-line no-unused-vars
+
 pool.on('error', (err, client) => {
   console.error('Error: ', err);
 });
+
 module.exports = {
   pool,
 };
